fde3f515af4730d5df1bf15ae25e20573fe5f909
ZEN8890 <Steven.gunawan8@hotmail.com>
2025-08-18 08:54:15 +0700

    updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:Strata_lite/screens/admin_dashboard_screen.dart';
-import 'package:Strata_lite/screens/staff_dashboard_screen.dart'; // <--- Import StaffDashboardScreen
+import 'package:Strata_lite/screens/staff_dashboard_screen.dart';
 import 'package:Strata_lite/screens/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:Strata_lite/firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart'; // <--- Import Cloud Firestore
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,40 +15,9 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final FirebaseFirestore _firestore =
-      FirebaseFirestore.instance; // <--- Instance Firestore
-
-  String initialRoute;
-  User? currentUser = _auth.currentUser;
-
-  if (currentUser != null) {
-    // Ambil role pengguna dari Firestore
-    try {
-      DocumentSnapshot userDoc =
-          await _firestore.collection('users').doc(currentUser.uid).get();
-      if (userDoc.exists) {
-        String role = (userDoc.data() as Map<String, dynamic>)['role'] ??
-            'staff'; // Default ke staff jika role tidak ada
-        if (role == 'admin') {
-          initialRoute = '/admin_dashboard';
-        } else {
-          initialRoute = '/staff_dashboard'; // Arahkan ke staff dashboard
-        }
-      } else {
-        // Jika dokumen user tidak ditemukan, perlakukan sebagai staff atau arahkan ke login
-        initialRoute = '/staff_dashboard';
-        print(
-            'Warning: User document not found for UID: ${currentUser.uid}, defaulting to staff dashboard.');
-      }
-    } catch (e) {
-      print('Error fetching user role: $e, defaulting to staff dashboard.');
-      initialRoute =
-          '/staff_dashboard'; // Fallback jika ada error saat mengambil role
-    }
-  } else {
-    initialRoute = '/';
-  }
+  // Perubahan: Langsung set rute awal ke halaman login,
+  // mengabaikan status login pengguna sebelumnya.
+  String initialRoute = '/';
 
   SystemChrome.setEnabledSystemUIMode(
     SystemUiMode.immersiveSticky,
@@ -80,8 +49,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => const LoginScreen(),
         '/admin_dashboard': (context) => const AdminDashboardScreen(),
-        '/staff_dashboard': (context) =>
-            const StaffDashboardScreen(), // <--- Tambahkan rute ini
+        '/staff_dashboard': (context) => const StaffDashboardScreen(),
       },
       debugShowCheckedModeBanner: false,
     );
